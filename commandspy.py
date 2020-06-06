9cad38534a7da099c8ecf1dcb8fbef81cbfc9dcb
Iván <brehynnermraz@gmail.com>
2020-06-05 21:20:11 -0500

    Fixing migrations"

--- commandspy.py
+++ commandspy.py
@@ -1,7 +1,12 @@
-backendvenv\Scripts\activate
+#backendvenv\Scripts\activate
 
 python -m pip install Django
 
 py manage.py runserver
 
-django-admin startproject polls
+django-admin startproject backend
+
+python manage.py startapp mathis
+
+##actulizar tablas
+python manage.py makemigrations mathis
